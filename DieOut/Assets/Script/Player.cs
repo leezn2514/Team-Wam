cddb2a37c68046d80715813d3cb34f23d506fc63
아현 이 <ryunyu2514@gmail.com>
2022-07-19 19:58:47 +0900

    [2022-07-19] Player Idle, Walk Animation

--- DieOut/Assets/Script/Player.cs
+++ DieOut/Assets/Script/Player.cs
@@ -5,31 +5,44 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private SpriteRenderer spriteRenderer;
+    private Animator player_ani;
 
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        player_ani = GetComponent<Animator>();
     }
 
     void Update()
     {
+        player_ani.SetBool("is_Walk", false);
+
         if (Input.GetKey(KeyCode.RightArrow))
         {
             this.transform.Translate(Vector2.right * Time.deltaTime * 10.0f);
             spriteRenderer.flipX = true;
+
+            player_ani.SetBool("is_Walk", true);
+
         }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             this.transform.Translate(Vector2.left * Time.deltaTime * 10.0f);
             spriteRenderer.flipX = false;
+
+            player_ani.SetBool("is_Walk", true);
         }
         if (Input.GetKey(KeyCode.UpArrow))
         {
             this.transform.Translate(Vector2.up * Time.deltaTime * 10.0f);
+            player_ani.SetBool("is_Walk", true);
+
         }
         if (Input.GetKey(KeyCode.DownArrow))
         {
             this.transform.Translate(Vector2.down * Time.deltaTime * 10.0f);
+            player_ani.SetBool("is_Walk", true);
+
         }
     }
 }
